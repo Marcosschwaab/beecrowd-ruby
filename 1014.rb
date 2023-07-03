6d0c39e5d8b5b29652535bf2a7f9d6282975711f
Marcos Schwaab <marcos.schwaab@gmail.com>
2023-07-03 12:37:45 -0300

    add 1014...1016

---
+++ 1014.rb
@@ -0,0 +1,6 @@
+x = gets.to_i
+y = gets.to_f
+
+consumo_medio = x / y
+
+puts format('%.3f km/l', consumo_medio)
---
+++ 1016.rb
@@ -0,0 +1,5 @@
+distancia = gets.to_i
+
+tempo = (distancia * 2).to_i
+
+puts "#{tempo} minutos"
