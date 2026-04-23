9473f16ef7a8ae619c80864daa60d4c65a62c47a
Marcos Schwaab <marcos.schwaab@gmail.com>
2026-04-23 09:40:03 -0300

    correction exercise 1045

--- 1045.rb
+++ 1045.rb
@@ -1,34 +1,24 @@
-def classifica_triangulo(a, b, c)
-    # Ordena os lados em ordem decrescente
-    lados = [a, b, c].sort.reverse
-    a, b, c = lados[0], lados[1], lados[2]
-  
-    # Verifica se forma um triângulo
-    if a >= b + c
-      puts 'NAO FORMA TRIANGULO'
-    else
-      # Verifica o tipo de triângulo
-      if a**2 == b**2 + c**2
-        puts 'TRIANGULO RETANGULO'
-      elsif a**2 > b**2 + c**2
-        puts 'TRIANGULO OBTUSANGULO'
-      else
-        puts 'TRIANGULO ACUTANGULO'
-      end
-  
-      # Verifica se é equilátero ou isósceles
-      if a == b && b == c
-        puts 'TRIANGULO EQUILATERO'
-      elsif a == b || b == c || a == c
-        puts 'TRIANGULO ISOSCELES'
-      end
-    end
+require 'bundler/setup' if defined?(Bundler)
+
+a, b, c = gets.chomp.split.map(&:to_f).sort.reverse # ordenar decrescente (A maior)
+
+puts "NAO FORMA TRIANGULO" if a >= b + c
+
+# Verificações apenas se formar triângulo
+if !(a >= b + c)
+  # Tipos de triângulo pelo ângulo
+  if a**2 == b**2 + c**2
+    puts "TRIANGULO RETANGULO"
+  elsif a**2 > b**2 + c**2
+    puts "TRIANGULO OBTUSANGULO"
+  else
+    puts "TRIANGULO ACUTANGULO"
   end
-  
-  # Exemplo de uso
-  classifica_triangulo(7.0, 5.0, 7.0)
-  classifica_triangulo(6.0, 6.0, 10.0)
-  classifica_triangulo(6.0, 6.0, 6.0)
-  classifica_triangulo(5.0, 7.0, 2.0)
-  classifica_triangulo(6.0, 8.0, 10.0)
-  
+
+  # Tipos de triângulo pelos lados (apenas se formar)
+  if a == b && b == c
+    puts "TRIANGULO EQUILATERO"
+  elsif a == b || b == c || a == c
+    puts "TRIANGULO ISOSCELES"
+  end
+end
